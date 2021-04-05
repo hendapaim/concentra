69fa65b9b51859642e33abdd9cf8dca642cbd515
HendaPaim <hendapaim278@gmail.com.com>
2021-04-06 00:12:57 +0100

    correcção de bugs

--- main.js
+++ main.js
@@ -3,6 +3,44 @@ function $(el)
     return document.querySelector(el);
 }
 
+// Zona de Execução do ConcentraH //
+// # Execução de test
+let horas = 1;
+let minutos = 30;
+let segundos = 60;
+
+const concentraTempo = new TemporizadorH(function() {
+    if(segundos === 0)
+    {
+        segundos = 60;
+        minutos -= 1
+    }else if(minutos === 0)
+    {
+        minutos = 60;
+        horas -= 1;
+    }else if(horas === 0)
+    {
+        console.log("Tempo acabou!")
+    }
+    
+    segundos--; 
+    $(".s").innerHTML = segundos;
+    $(".m").innerHTML = minutos +":";
+    $(".h").innerHTML = horas +":";
+    console.log(segundos);
+}, 1000);
+
+$(".inicio").addEventListener("click", function(){
+    concentraTempo.iniciar();
+});
+$(".pausa").addEventListener("click", function(){
+    concentraTempo.pausar()
+});
+$(".cancelar").addEventListener("click", function(){
+    concentraTempo.cancelar()
+});
+//console.log(concentraTempo.inicio)
+
 function TemporizadorH(callback, tempo)
 {
     let tempoID;
@@ -31,44 +69,4 @@ function TemporizadorH(callback, tempo)
     this.iniciar = iniciar;
     this.pausar = pausar;
     this.cancelar = cancelar;
-}
-
-// Zona de Execução do ConcentraH //
-
-    // # Execução de test
-    let horas = 1;
-    let minutos = 30;
-    let segundos = 60;
-
-    const concentraTempo = new TemporizadorH(function() {
-        if(segundos === 0)
-        {
-            segundos = 60;
-            minutos -= 1
-        }else if(minutos === 0)
-        {
-            minutos = 60;
-            horas -= 1;
-        }else if(horas === 0)
-        {
-            console.log("Tempo acabou!")
-        }
-        
-        segundos--; 
-        $(".s").innerHTML = segundos;
-        $(".m").innerHTML = minutos +":";
-        $(".h").innerHTML = horas +":";
-        console.log(segundos);
-    }, 1000);
-
-    $(".inicio").addEventListener("click", function(){
-        concentraTempo.iniciar();
-    });
-    $(".pause").addEventListener("click", function(){
-        concentraTempo.pausar();
-    });
-    $(".cancelar").addEventListener("click", function(){
-        concentraTempo.cancelar();
-    });
-
-    //console.log(concentraTempo.inicio)
+} 
