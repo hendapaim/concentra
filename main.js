119f6d02974253a1ee11e98a12dd46dd4c0b8f04
HendaPaim <hendapaim278@gmail.com.com>
2021-04-06 19:19:16 +0100

    nav menu

--- main.js
+++ main.js
@@ -3,30 +3,36 @@ function $(el)
     return document.querySelector(el);
 }
 
+function hora(){
+
+}
+
 // Zona de Execução do ConcentraH //
 // # Execução de test
-let horas = 1;
-let minutos = 30;
-let segundos = 60;
+let horas = 0;
+let minutos = 1;
+let segundos = 00;
 
 const concentraTempo = new TemporizadorH(function() {
-    if(segundos === 0)
+    if(segundos === 0 && minutos != 0)
     {
-        segundos = 60;
         minutos -= 1
-    }else if(minutos === 0)
+        segundos = 60;
+    }else if(minutos === 0 && horas != 0)
     {
-        minutos = 60;
         horas -= 1;
-    }else if(horas === 0)
-    {
-        console.log("Tempo acabou!")
+        minutos = 60;
+    }else if(horas === 0 && minutos === 0 && segundos === 0)
+    {   
+        console.log("Tempo acabou!");
+        alert("Tempo acabou");
     }
     
     segundos--; 
-    $(".s").innerHTML = segundos;
-    $(".m").innerHTML = minutos +":";
-    $(".h").innerHTML = horas +":";
+    
+    $(".s").innerHTML = (segundos < 10) ? ("0" + segundos) : segundos;
+    $(".m").innerHTML = ((minutos < 10) ? ("0" + minutos) : minutos) + ":";
+    $(".h").innerHTML = `${horas}:`;
     console.log(segundos);
 }, 1000);
 
