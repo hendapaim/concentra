6d1dfa156660bae4c207a71243adf2a4b8a3c194
HendaPaim <hendapaim278@gmail.com.com>
2021-04-18 01:19:42 +0100

    mudança

--- main.js
+++ main.js
@@ -2,59 +2,80 @@ function $(el)
 {
     return document.querySelector(el);
 }
-$('.relogio').innerHTML = Date().slice(16, 21);
+
 const tempo = 25;
-let tempoID,
-    minutos,
-    segundos = "00",
-    contagem = false,
+if(tempo<=999){
 
-    tempoDecorrido = tempo*60;
+    let tempoID,
+        minutos,
+        segundos,
+        contagem = false,
+        tempoEmSegundos =tempo*60,
+        tempoDecorrido = tempoEmSegundos;
 
-$('.display').innerHTML = `${tempo}:${segundos}`;
+    function display(tempoDecorrido) {
+        minutos = (tempoDecorrido / 60) | 0;
+        segundos = (tempoDecorrido % 60) | 0;
 
-function pausar()
-{
-    clearInterval(tempoID);
-}
-function cancelar()
-{
-    clearInterval(tempoID);
-    tempoDecorrido = tempo*60;
-    $('.display').innerHTML = `${tempo}:${segundos}`;
-}
-function iniciar()
-{
-    tempoID = setInterval(() => temporizador(), 1000);
-}
-function temporizador()
-{
-    // horas = (tempoDecorrido / 3600) | 0;
-    // minutos = (tempoDecorrido / 60) % 60 | 0;
-    minutos = (tempoDecorrido / 60) | 0;
-    segundos = (tempoDecorrido % 60) | 0;
+        minutos = minutos < 10 ? "0" + minutos : minutos;
+        segundos = segundos < 10 ? "0" + segundos : segundos;
+        $('.relogio').innerHTML = Date().slice(16, 21);
+        $('.display').innerHTML = `${minutos}:${segundos}`;
+    }
+        
+    //$('.display').innerHTML = `${tempo}:${segundos}`;
+    display(tempoDecorrido);
 
-    minutos = minutos < 10 ? "0" + minutos : minutos;
-    segundos = segundos < 10 ? "0" + segundos : segundos;
-
-    $('.display').innerHTML = `${minutos}:${segundos}`;
-    tempoDecorrido--;
-    if (tempoDecorrido < 0)
+    function pausar()
     {
         clearInterval(tempoID);
-        alert("Tempo esgotou! alivia-se por 5 minutos depois volte o PomoCentra");
-        tempoDecorrido = tempo*60;
-        return;
     }
-    //console.log(tempoDecorrido)
+    function cancelar()
+    {
+        clearInterval(tempoID);
+        tempoDecorrido = tempoEmSegundos;
+        $('.display').innerHTML = `${tempo}:${segundos}`;
+    }
+    function iniciar()
+    {
+        tempoID = setInterval(() => temporizador(), 1000);
+    }
+    function temporizador()
+    {
+        // horas = (tempoDecorrido / 3600) | 0;
+        // minutos = (tempoDecorrido / 60) % 60 | 0;
+        display(tempoDecorrido);
+
+        tempoDecorrido--;
+        if (tempoDecorrido < -1)
+        {
+            clearInterval(tempoID);
+            alert("Tempo esgotou! alivia-se por 5 minutos depois volte o PomoCentra");
+            contagem = true;
+            tempoDecorrido = tempoEmSegundos;
+            display(tempoDecorrido);
+            $(".pausa").style.display="none";
+            $(".inicio").style.display="block";
+            return;
+        }
+        //console.log(tempoDecorrido)
+    }
+
+    $(".inicio").addEventListener("click", function(){
+        iniciar();
+        $(".inicio").style.display="none";
+        $(".pausa").style.display="block";
+    });
+    $(".pausa").addEventListener("click", function(){
+        pausar();
+        $(".pausa").style.display="none";
+        $(".inicio").style.display="block";
+    });
+    /*
+    $("#cancelar").addEventListener("click", function(){
+        cancelar();
+    });
+    */
+}else{
+    alert("O tempo deve ser menor que 999 minutos!")
 }
-$(".inicio").addEventListener("click", function(){
-    iniciar();
-});
-/*$(".pausa").addEventListener("click", function(){
-    pausar();
-});
-*/
-$("#cancelar").addEventListener("click", function(){
-    cancelar();
-});
